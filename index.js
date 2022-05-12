6c5374332fd9a7e682263f457fa5b8d2bd1dc3b6
AMAECHISIXTUS <sixtus.collins@yahoo.com>
2022-05-12 09:22:10 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const navToggle = document.querySelector(".nav__toggle");
 const navLinks = document.querySelectorAll('.nav__link')
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener('click', function() {
     document.body.classList.toggle('nav-open');
 });
 
